977292ec97cd95809d897546fa635f5ba16da3e5
Sammy Hass <sammyhass01@gmail.com>
2022-10-11 16:27:37 +0100

    remove tinygo download after install complete

--- scripts/install-tinygo.sh
+++ scripts/install-tinygo.sh
@@ -4,4 +4,5 @@ echo "Install TinyGo (AMD64)"
 wget https://github.com/tinygo-org/tinygo/releases/download/v0.26.0/tinygo_0.26.0_amd64.deb
 dpkg -i tinygo_0.26.0_amd64.deb
 export PATH=$PATH:/usr/local/tinygo/bin
+rm tinygo_0.26.0_amd64.deb
 echo "Finished installing TinyGo: $(tinygo version)"
