c72004f33419ed78c9f399c6152e75b8f70ec625
Sammy Hass <sammyhass01@gmail.com>
2022-11-10 19:04:38 +0000

    fix cors

--- modules/router/router.go
+++ modules/router/router.go
@@ -60,7 +60,7 @@ func (r Router) Middleware() {
 	r.Engine.Use(
 		cors.New(
 			cors.Config{
-				AllowOrigins: []string{"http://localho.st:3000"},
+				AllowOrigins: []string{"http://localho.st:3000", "https://wasm-web-ide-client.vercel.app"},
 				AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
 				AllowHeaders: []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
 			},
