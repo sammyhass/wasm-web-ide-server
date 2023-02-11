5fce4486527c855aedeb235df5ccc8582e435d71
Sammy Hass <sammyhass01@gmail.com>
2023-02-11 16:07:11 +0000

    tests: fix tests

--- scripts/install-asc.sh
+++ scripts/install-asc.sh
@@ -1,5 +1,6 @@
-curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
-apt-get install -y nodejs
-node -v
-npm install -g assemblyscript
-asc --version
+curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - &&
+	apt-get install -y nodejs &&
+	node -v &&
+	npm -v &&
+	npm install -g assemblyscript &&
+	asc --version
